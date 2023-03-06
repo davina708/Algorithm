948633d1cffcb502a1daee3fa259e23e84f833ce
Davina <111004706+davina708@users.noreply.github.com>
2023-03-06 23:52:41 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,28 +1,15 @@
-function solution(numer1, denom1, numer2, denom2) {
-    let arr = [];
-    let gcd=1;
-    let lcm=denom1*denom2;
-    let num=Math.max(denom1,denom2)
-    for(let i=2;i<=num;i++){
-        if(denom1%i===0&&denom2%i===0){
-            gcd=i;
+function solution(denum1, num1, denum2, num2) {
+    // 분자
+    let topNum = num1*denum2 + num2*denum1
+    // 분모
+    let botNum = num1*num2
+    // 최소 공배수
+    let maximum = 1
+    // 약분
+    for(let i = 2 ; i <= topNum ; i ++) {
+        if(topNum%i === 0 && botNum%i === 0) {
+            maximum = i
         }
     }
-    lcm=lcm/gcd;
-    arr[0]=(numer1*(lcm/denom1))+(numer2*(lcm/denom2))
-    arr[1]=lcm;
-    let sm=1;
-    let num2=Math.min(arr[0],arr[1])
-    for(let i=2;i<=num2;i++){
-        if(arr[0]%i===0&&arr[1]%i===0){
-            sm=i;
-        }
-    }
-    return [arr[0]/sm,arr[1]/sm];
+    return [topNum/maximum, botNum/maximum]
 }
-
-//1. denom1 & denom2 의 최소공배수를 구한다
-//2. 최소공배수 % denom1을 numer1에 곱하고 
-//3. 최소공배수 % denom2을 numer2에 곱하고 
-//4. 두 수의 합이 첫번째 인자, 
-//5. 최소공배수가 두번째 인자로 배열 return 
