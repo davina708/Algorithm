9db7acc8590212ea880fc67be44b219b2687786c
Davina <111004706+davina708@users.noreply.github.com>
2023-03-09 19:37:13 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,8 +1,9 @@
 function solution(money) {
-    let count=0;
-    while(money>=5500){
-        money=money-5500;
-        count++;
-    }
-       return [count,money]
+    // let count=0;
+    // while(money>=5500){
+    //     money=money-5500;
+    //     count++;
+    // }
+    //    return [count,money]
+    return [Math.floor(money/5500), money%5500]
 }
