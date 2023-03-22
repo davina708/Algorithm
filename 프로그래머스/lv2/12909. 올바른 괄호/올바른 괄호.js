2348f93b6dd878ae5df3e9a05273f495bc8b99fd
Davina <111004706+davina708@users.noreply.github.com>
2023-03-22 16:13:03 +0900

    [level 2] Title: 올바른 괄호, Time: 9.63 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,15 @@
+function solution(s){
+    let stack=[];
+    for(let i=0;i<s.length;i++){
+        if(s[i]==="("){
+            stack.push(s[i])
+        } else if(s[i]===")"){
+            if(stack[stack.length-1]==="("){
+                stack.pop();
+            } else return false;
+        }
+    }
+    if(stack.length===0){
+        return true;
+    } else return false;
+}
